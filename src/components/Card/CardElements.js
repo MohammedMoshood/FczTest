c6efc88f8562bcf87c153c5705948baba4cd829a
MohammedMoshood <moshood.mm8@gmail.com>
2022-08-02 15:29:16 +0100

    CardComponent completed

--- src/components/Card/CardElements.js
+++ src/components/Card/CardElements.js
@@ -12,30 +12,43 @@ export const CardContainer = styled.div`
   z-index: 1;
 `;
 export const CardBg = styled.div`
-width: 65%;
-height: 550px;
-background: lightgrey;
-border-radius: 50px;
-box-shadow: 5px 5px 50px black;
-cursor: pointer;
-align-content: center;
-justify-content: center;
+  width: 65%;
+  height: 550px;
+  background: lightgrey;
+  border-radius: 50px;
+  box-shadow: 5px 5px 50px black;
+  cursor: pointer;
+  align-content: center;
+  justify-content: center;
 
-&:hover{
+  &:hover {
     background: grey;
-    transition:0.4s ease-in-out ;
-}
-
-`
+    transition: 0.4s ease-in-out;
+  }
+`;
 export const CardDim = styled.div`
-  width: 98%; 
+  width: 98%;
   height: 530px;
-  margin:  10px 12px;
+  margin: 10px 12px;
   padding: 10px;
   position: relative;
   border-radius: 50px;
   background: white;
+  @media screen and (max-width: 768px) {
+    margin: 10px 5px;
+    &:hover {
+      width: 95%;
+      height: 510px;
+      margin: 20px 0.8rem;
+      transition: 0.4s ease-in-out;
+    }
+  }
+  &:hover {
+    width: 95%;
+    height: 510px;
+    margin: 20px auto;
+    transition: 0.4s ease-in-out;
+  }
 `;
-export const CardComp = styled.img`
+export const CardComp = styled.img``;
 
-`
