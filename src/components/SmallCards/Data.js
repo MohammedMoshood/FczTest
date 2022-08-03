cb2efcde2205f647f4c1cd642a00689f01db0139
MohammedMoshood <moshood.mm8@gmail.com>
2022-08-03 09:56:33 +0100

    updated card image and unnecessary file deletion

--- src/components/SmallCards/Data.js
+++ src/components/SmallCards/Data.js
@@ -1,26 +1,26 @@
 export const smallCard1 = {
-  Img: require("../../image/contraction.png"),
+  Img: require("../../image/profits.png"),
   altText: "Cash",
   Heading: "Contraction of Loans and Bond Issuance",
   subtitle:
     "issuance on behalf of the state , such instruments as the state may desire to issue , from time to time.",
   id: "1",
-  
 };
 
 export const smallCard2 = {
   Img: require("../../image/edit.png"),
   altText: "Penandpaper",
   Heading: "Management of Lagos State Debt Obligations",
-  subtitle: "prepare and implement a plan for the efficient management of state's debt obligations at sustainable levels",
-  id:"2",
- 
+  subtitle:
+    "prepare and implement a plan for the efficient management of state's debt obligations at sustainable levels",
+  id: "2",
 };
 
 export const smallCard3 = {
   Img: require("../../image/check.png"),
   altText: "check",
-  Heading:"Forecast of Loan Servicing",
-  subtitle:"prepare and submit to the government, a forecast of loan service obligations for each financial year.",
-  id:"3",
+  Heading: "Forecast of Loan Servicing",
+  subtitle:
+    "prepare and submit to the government, a forecast of loan service obligations for each financial year.",
+  id: "3",
 };
